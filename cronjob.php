d7877b23ff5f568e9af2d5e0a37470da24482617
tom2tom <tpgww@onepost.net>
2015-04-14 18:42:33 +1000

    test to eleminate external 'hit'

--- cronjob.php
+++ cronjob.php
@@ -1,7 +1,11 @@
 <?php
+
+global $gCms;
+if (!isset ($gCms)) exit;
+
 require ('../../config.php');
 
 $cronurl = $config['root_url'] . '/modules/Cron/cron/m1_';
 
-echo file_get_contents($cronurl);
+echo file_get_contents ($cronurl);
 ?>
