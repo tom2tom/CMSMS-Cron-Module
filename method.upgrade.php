f2e5fe97ef8499694a1819d3db2f0ca66b111b1e
tom2tom <tpgww@onepost.net>
2015-04-14 18:34:02 +1000

    include license text

--- method.upgrade.php
+++ method.upgrade.php
@@ -1,4 +1,10 @@
 <?php
+#----------------------------------------------------------------------
+# This file is part of CMS Made Simple module: Cron
+# Copyright (C) 2010-2015 Jean-Christophe Cuvelier <jcc@atomseeds.com>
+# Refer to licence and other details at the top of file Cron.module.php
+# More info at http://dev.cmsmadesimple.org/projects/cron
+#----------------------------------------------------------------------
 
 switch($$oldversion)
 {
