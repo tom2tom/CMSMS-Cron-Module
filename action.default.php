09e40bb41a2c1210705a6992c6323d6d78f10b3d
tom2tom <tpgww@onepost.net>
2015-04-14 18:38:06 +1000

    various updates

--- action.default.php
+++ action.default.php
@@ -1,17 +1,32 @@
 <?php
+#----------------------------------------------------------------------
+# This file is part of CMS Made Simple module: Cron
+# Copyright (C) 2010-2015 Jean-Christophe Cuvelier <jcc@atomseeds.com>
+# Refer to licence and other details at the top of file Cron.module.php
+# More info at http://dev.cmsmadesimple.org/projects/cron
+#----------------------------------------------------------------------
 
-if (!cmsms()) exit;
+global $gCms;
+if (!isset ($gCms)) exit;
+
+if (!empty($params['showtemplate']))
+{
+	//do what stuff ??
+}
 
 $now = time();
-$periods = $this->getPeriods();
+$periods = cron_utils::getPeriods();
 foreach ($periods as $period => $time)
 {
-	$last = $this->GetPreference('Last'.$period);
-	if ($last <= strtotime($time, $now))
+	$last = $this->GetPreference ('Last'.$period);
+	if ($last <= strtotime ($time, $now))
 	{
-		$this->SetPreference('Last'.$period, $now);
-		$this->SendEvent('Cron'.$period,array());
+		$this->SetPreference ('Last'.$period, $now);
+		$this->SendEvent ('Cron'.$period, array ($now));
 	}
 }
+
+if (isset ($params['manual']))
+	$this->Redirect ($id, 'defaultadmin'); 
 
 ?>
