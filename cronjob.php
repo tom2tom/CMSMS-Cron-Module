d04deb2f97d38a4cbcdeaa685123e4c4499514e7
tom2tom <tpgww@onepost.net>
2015-04-15 08:03:41 +1000

    bugfix

--- cronjob.php
+++ cronjob.php
@@ -1,11 +1,8 @@
 <?php
-
-global $gCms;
-if (!isset ($gCms)) exit;
-
-require ('../../config.php');
-
-$cronurl = $config['root_url'] . '/modules/Cron/cron/m1_';
-
-echo file_get_contents ($cronurl);
+// CHECKME what is this for ?
+if (!cron_utils::isme()) exit;
+$config = cmsms()->GetConfig();
+$fn = cms_join_path ($config['root_path'],'modules','Cron','cron','m1_');
+if (is_file ($fn))
+	echo file_get_contents ($fn);
 ?>
