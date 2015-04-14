5208eafddef23d59bff201e5ec351ea2385765e4
tom2tom <tpgww@onepost.net>
2015-04-14 18:54:21 +1000

    cleanup

--- lang/en_US.php
+++ lang/en_US.php
@@ -1,30 +1,39 @@
 <?php
+#----------------------------------------------------------------------
+# This file is part of CMS Made Simple module: Cron
+# Copyright (C) 2010-2015 Jean-Christophe Cuvelier <jcc@atomseeds.com>
+# Refer to licence and other details at the top of file Cron.module.php
+# More info at http://dev.cmsmadesimple.org/projects/cron
+#----------------------------------------------------------------------
 
-$lang['friendlyname'] = 'Cron';
-$lang['postinstall'] = 'Installed';
-$lang['postuninstall'] = 'Successfully uninstalled';
-$lang['really_uninstall'] = 'Really? Are you sure you want to unsinstall the Cron module?';
-$lang['uninstalled'] = 'Module uninstalled.';
-$lang['installed'] = 'Module version %s installed.';
-$lang['upgraded'] = 'Module upgraded to version %s.';
-$lang['moddescription'] = 'This module allows execution of cron jobs for modules.';
-
-$lang['run_cron'] = 'Send';
-$lang['run_tip'] = 'send all pending Cron* events to registered handlers';
-$lang['cron_executed'] = '%s cron successfully executed';
-$lang['cron_launched'] = 'Cron task launched';
+$lang['accessdenied'] = 'Access denied. Please check your permissions.';
+$lang['admindescription'] = 'This module provides a convenient way for other modules to get periodic notifications, of several kinds.';
+$lang['areyousure'] = 'Are you sure ?';
 
 $lang['error'] = 'Error!';
-$lang['admindescription'] = 'This module provides a convenient way for other modules to get periodic notifications, of several kinds.';
-$lang['accessdenied'] = 'Access denied. Please check your permissions.';
-$lang['postinstall'] = 'Cron module successfully installed. After setting relevant permissions, read the module help to see what to do next.';
 
-$lang['perm_review'] = 'Review status of sent Cron events';
-$lang['perm_send'] = 'Manually send Cron events';
+$lang['friendlyname'] = 'Cron';
+
+$lang['help_showtemplate'] = 'TODO';
+
+$lang['last_used'] = 'Last sent on (Y-m-d)';
 
 $lang['period'] = 'Period';
-$lang['last_used'] = 'Last sent on (Y-m-d)';
-$lang['areyousure'] = 'Are you sure ?';
+$lang['perm_review'] = 'Review status of sent Cron events';
+$lang['perm_send'] = 'Manually send Cron events';
+$lang['postinstall'] = 'Cron module successfully installed. After setting relevant permissions, read the module help to see what to do next.';
+$lang['postuninstall'] = 'Cron module successfully uninstalled';
+
+$lang['really_uninstall'] = 'Really? Are you sure you want to unsinstall the Cron module?';
+$lang['run_cron'] = 'Send';
+$lang['run_tip'] = 'send all pending Cron* events to registered handlers';
+
+$lang['send_frequency'] = 'Sent at intervals of approximately %s';
+
+$lang['timeparameter'] = 'This event supplies one parameter: a timestamp representing the date/time sent';
+
+$lang['unused'] = 'Never';
+$lang['upgraded'] = 'Module upgraded to version %s.';
 
 $lang['15min'] = 'Quarter-hourly';
 $lang['Hourly'] = 'Hourly';
@@ -33,7 +42,6 @@ $lang['Weekly'] = 'Weekly';
 $lang['Monthly'] = 'Monthly';
 $lang['Yearly'] = 'Yearly';
 
-$lang['send_frequency'] = 'Sent at intervals of approximately %s';
 $lang['period_qtrhr'] = '15 minutes';
 $lang['period_hour'] = 'an hour';
 $lang['period_day'] = 'a day';
@@ -47,17 +55,6 @@ $lang['event_Daily_desc'] = sprintf($lang['send_frequency'],$lang['period_day'])
 $lang['event_Weekly_desc'] = sprintf($lang['send_frequency'],$lang['period_week']);
 $lang['event_Monthly_desc'] = sprintf($lang['send_frequency'],$lang['period_month']);
 $lang['event_Yearly_desc'] = sprintf($lang['send_frequency'],$lang['period_year']);
-/*
-$lang['event_15min_help'] = '';
-$lang['event_Hourly_help'] = '';
-$lang['event_Daily_help'] = '';
-$lang['event_Weekly_help'] = '';
-$lang['event_Monthly_help'] = '';
-$lang['event_Yearly_help'] = ''; 
-*/
-//$lang['noparameter'] = 'No event parameters';
-$lang['timeparameter'] = 'This event supplies one parameter: a timestamp representing the date/time sent';
-$lang['unused'] = 'Never';
 
 $lang['help_module'] = <<<EOS
 <h3>What Does It Do?</h3>
@@ -116,7 +113,5 @@ public function DoEvent(\$originator, \$eventname, &amp;\$params)
 <p>Copyright &copy; 2010-2015 Jean-Christophe Cuvelier &lt;<a href="mailto:jcc@morris-chapman.com">jcc@morris-chapman.com</a>&gt;. All rights reserved.</p>
 <p>This module has been released under the <a href="http://www.gnu.org/licenses/licenses.html#AGPL">GNU Affero General Public License</a> version 3. The module may not be used otherwise than in accordance with the terms of that license.</p>
 EOS;
-
-$lang['help_showtemplate'] = 'TODO';
 
 ?>
