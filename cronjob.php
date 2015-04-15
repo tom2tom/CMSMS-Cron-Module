e4024357267bd1c49655d7a1dcc429eda8472794
tom2tom <tpgww@onepost.net>
2015-04-16 08:15:31 +1000

    adjust files

--- cronjob.php
+++
@@ -1,8 +0,0 @@
-<?php
-// CHECKME what is this for ?
-if (!cron_utils::isme()) exit;
-$config = cmsms()->GetConfig();
-$fn = cms_join_path ($config['root_path'],'modules','Cron','cron','m1_');
-if (is_file ($fn))
-	echo file_get_contents ($fn);
-?>
